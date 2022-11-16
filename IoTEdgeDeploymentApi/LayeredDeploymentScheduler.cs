c72f29fa616d11df9c0a7e74fcbac3eb5cc6551e
Marcel Bindseil <marcbind@microsoft.com>
2022-11-16 15:27:03 +0100

    added scheduler called func

--- IoTEdgeDeploymentApi/LayeredDeploymentScheduler.cs
+++ IoTEdgeDeploymentApi/LayeredDeploymentScheduler.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using IoTEdgeDeploymentEngine;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -6,9 +8,21 @@ namespace IoTEdgeDeploymentApi
 {
     public class LayeredDeploymentScheduler
     {
-        [FunctionName("LayeredDeploymentScheduler")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer)
+        private readonly IoTEdgeLayeredDeploymentBuilder _ioTEdgeLayeredDeploymentBuilder;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="ioTEdgeLayeredDeploymentBuilder">IoTEdgeLayeredDeploymentBuilder instance per DI</param>
+        public LayeredDeploymentScheduler(IoTEdgeLayeredDeploymentBuilder ioTEdgeLayeredDeploymentBuilder)
         {
+            _ioTEdgeLayeredDeploymentBuilder = ioTEdgeLayeredDeploymentBuilder;
+        }
+        
+        [FunctionName("LayeredDeploymentScheduler")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer)
+        {
+            await _ioTEdgeLayeredDeploymentBuilder.ApplyDeployments();
         }
     }
 }
