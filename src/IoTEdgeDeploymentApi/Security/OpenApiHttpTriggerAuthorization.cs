45507772ff72c28dc3a9962b04ce2d7731781c23
Marcel Bindseil <marcbind@microsoft.com>
2022-12-08 16:32:09 +0100

    added comments

--- src/IoTEdgeDeploymentApi/Security/OpenApiHttpTriggerAuthorization.cs
+++ src/IoTEdgeDeploymentApi/Security/OpenApiHttpTriggerAuthorization.cs
@@ -6,10 +6,17 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
 
 namespace IoTEdgeDeploymentApi.Security
 {
+    /// <summary>
+    /// Open Api Http Trigger Auth that returns a response depending on claims
+    /// </summary>
     public class OpenApiHttpTriggerAuthorization : DefaultOpenApiHttpTriggerAuthorization
     {
+        /// <summary>
+        /// Http context accessor
+        /// </summary>
         public virtual IHttpContextAccessor HttpContextAccessor { get; set; }
 
+        /// <inheritdoc />
         public override async Task<OpenApiAuthorizationResult> AuthorizeAsync(IHttpRequestDataObject req)
         {
             var result = default(OpenApiAuthorizationResult);
@@ -17,6 +24,7 @@ namespace IoTEdgeDeploymentApi.Security
             var claims = this.HttpContextAccessor.HttpContext.User.Claims;
             if (!claims.Any())
             {
+                //todo: assess claims for Swagger UI auth
                 // result = new OpenApiAuthorizationResult()
                 // {
                 //     StatusCode = HttpStatusCode.Unauthorized,
--- src/IoTEdgeDeploymentApi/Startup.cs
+++ src/IoTEdgeDeploymentApi/Startup.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System;
+﻿using System;
+using IoTEdgeDeploymentApi;
 using IoTEdgeDeploymentEngine;
 using IoTEdgeDeploymentEngine.Accessor;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Devices;
-using IoTEdgeDeploymentApi.Security;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using OpenApiHttpTriggerAuthorization = IoTEdgeDeploymentApi.Security.OpenApiHttpTriggerAuthorization;
 
-[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+[assembly: FunctionsStartup(typeof(Startup))]
 
-namespace MyNamespace
+namespace IoTEdgeDeploymentApi
 {
 	/// <inheritdoc />
 	public class Startup : FunctionsStartup
